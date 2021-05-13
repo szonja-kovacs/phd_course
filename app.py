0626b2077bc657fc7155eea4b01f79fb6300148c
szonja-kovacs <80385966+szonja-kovacs@users.noreply.github.com>
2021-05-13 18:19:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 @st.cache
-def load.data
+def load.data():
     data = pd.read_csv('https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv')
     data['week'] = data.year_week.apply(lambda x: convert(x))
     return data
@@ -15,11 +15,16 @@ def convert(x):
 
 data = load_data()
 
-hun = data[data.country=='Hungary']
-
-fig = px.line(data_frame = hun, x = 'week', y = 'cumulative_count', color = 'indicator')
-
 st.title('covid-app')
 country = st.selectbox('Select a country', ['Hungary', 'Belgium'])
 st.write(f'The selected country is {country}')
 st.plotly_chart(fig)
+
+countries = data['country'].unique()
+countries_select= st.sidebar.selectbox('Select the country:', countries)
+
+fig = px.line(data_frame = data.country, x = 'week', y = 'cumulative_count', color = 'indicator')
+
+st.plotly_chart(fig)
+
+
